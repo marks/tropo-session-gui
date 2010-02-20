39c820de1f4e83ff566f71f0f128f93f78840417
Mark S <mark@mark-s.net>
2010-02-19 23:59:21 -0500

    cleaned up code, added screenshot, much better ;)

--- README.markdown
+++ README.markdown
@@ -2,6 +2,10 @@ Tropo Session API GUI
 =====================
 Built with Ruby, Shoes, and the great API at Tropo
 
+Screenshot
+----------
+![GUI Screenshot](screenshot.jpg)
+
 Don't forget your _config.yaml_ !
 ---------------------------------
 	---
--- gui.rb
+++ gui.rb
@@ -1,36 +1,29 @@
 %w(open-uri rexml/document yaml cgi).each {|lib| require lib}
 CONFIG = YAML::load(File.read('config.yaml'))
-
-call_options = {  'to' => "15128267004", 
+call_options = {  'to' => "12021231234", 
                   'network' => "sms",
                   'message' => "A Generic Message."    }  
 show_fields = call_options.keys # A.K.A. ["to","network","message"]
 user_set_fields = Hash.new
-
 Shoes.app :title => "Mark's Tropo Session API Script", :width => 320, :height => 500 do
-  background "Clouds.jpeg"
+  background "background.jpg"
   @content = stack :margin => 40, :margin_top => 15 do
   subtitle "Tropo Initiate", :margin => 5
   @notifications = stack do; end
   @field_stacks = Hash.new
-
   stack do
     show_fields.each do |field|
       stack :margin => 5 do 
         para field, :margin_bottom => 0, :underline => 'single'
         click do; @field_stacks[field].toggle; end
-        
         @field_stacks[field] = stack :margin => 0 do
           user_set_fields[field] = edit_line :text => call_options.values_at(field)
         end
-        
       end
     end
-    
     @buttons = flow :margin => 5 do
-      button "Go!", :stroke => 'green' do
+      button "Go!" do
             show_fields.each {|field| call_options[field] = user_set_fields[field].text}
-            
             if call_options['network'].upcase == 'VOICE'
               call_options['token'] = CONFIG['tropo']['session']['voice']
             else # text
@@ -40,8 +33,9 @@ Shoes.app :title => "Mark's Tropo Session API Script", :width => 320, :height =>
             params = "?action=create"
             call_options.each {|key,value| params << "&"+key+"="+CGI.escape(value.to_s)}
             alert(CONFIG['tropo']['session']['url']+params) # uncomment this whole line if you want to see that 'URL
-            response = open(api_url + params).read
+            response = open(CONFIG['tropo']['session']['url'] + params).read
             xml = REXML::Document.new(response) 
+            alert(xml)
             if xml.root.get_text("success") == true
               @notifications.prepend do
                 stack :margin => 10 do
@@ -68,10 +62,7 @@ Shoes.app :title => "Mark's Tropo Session API Script", :width => 320, :height =>
         alert "- You can click underlined text to toggle its content\r   view (i.e. field names, notifications)
              \n- For more information, contact @Skram at\r   mark@marksilver.net or \'SkramX\' on Freenode IRC"
       end
-      
     end
-    
-  end
-    
+  end 
   end
 end
