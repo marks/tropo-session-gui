c4cc52efcb306c868fefff89ed91341fdd5ec1ca
Mark S <mark@mark-s.net>
2010-02-20 19:45:37 -0500

    test and .gitignore changes

---
+++ test.rb
@@ -0,0 +1,7 @@
+%w(yaml open-uri rexml/document cgi helpers.rb).each {|lib| require lib}
+config = YAML::load(File.read('config.yaml'))
+
+call_options = {  'to' => "SkramX", 
+                  'network' => "aim",
+                  'message' => "A Generic Message."    }  
+new_session(call_options,config)
